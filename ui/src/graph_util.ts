eb00a27504ca61cff0592b2305441b2719f56fc0
Jonny Brooks <jonathan.leonard.brooks@gmail.com>
2018-09-22 23:55:32 +0100

    Changes in prep for tooltip update

--- ui/src/graph_util.ts
+++ ui/src/graph_util.ts
@@ -2,6 +2,53 @@
 * Graph utility functions
 * */
 export namespace GraphUtil {
+
+    /*
+    * Define the graph defaults
+    * */
+    export const defaultLayout = {
+        name: "cose-bilkent",
+        animate: false,
+        spacingFactor: 5
+    };
+
+    const defaultStyle = [
+        {
+            selector: "node",
+            style: {
+                height: 15,
+                width: 15,
+                "background-color": "data(colour)",
+                label: "data(label)",
+                "font-family": "Roboto, Helvetica, Arial, sans-serif",
+                "font-weight": "500",
+                "text-background-opacity": 0.5,
+                "text-background-color": "white",
+                "text-background-padding": "5px"
+            }
+        },
+        {
+            selector: ".selected",
+            style: {
+                "text-background-opacity": 0.6,
+                "text-background-color": "yellow",
+                "text-background-padding": "5px"
+            }
+        },
+        {
+            selector: "edge",
+            style: {
+                "curve-style": "bezier",
+                "width": 3,
+                "opacity": 0.8,
+                "target-arrow-shape": "triangle",
+                "target-arrow-color": "data(colour)",
+                "line-color": "data(colour)",
+                label: "data(label)"
+            }
+        }
+    ];
+
     /*
     * Generate config object for cytoscape's rendering engine
     * */
@@ -10,47 +57,8 @@ export namespace GraphUtil {
             container: container,
             boxSelectionEnabled: false,
             autounselectify: true,
-            layout: {
-                name: "cose-bilkent",
-                animate: false,
-                spacingFactor: 5
-            },
-            style: [
-                {
-                    selector: "node",
-                    style: {
-                        height: 15,
-                        width: 15,
-                        "background-color": "data(colour)",
-                        label: "data(label)",
-                        "font-family": "Roboto, Helvetica, Arial, sans-serif",
-                        "font-weight": "500",
-                        "text-background-opacity": 0.5,
-                        "text-background-color": "white",
-                        "text-background-padding": "5px"
-                    }
-                },
-                {
-                    selector: ".selected",
-                    style: {
-                        "text-background-opacity": 0.6,
-                        "text-background-color": "yellow",
-                        "text-background-padding": "5px"
-                    }
-                },
-                {
-                    selector: "edge",
-                    style: {
-                        "curve-style": "bezier",
-                        "width": 3,
-                        "opacity": 0.8,
-                        "target-arrow-shape": "triangle",
-                        "target-arrow-color": "data(colour)",
-                        "line-color": "data(colour)",
-                        label: "data(label)"
-                    }
-                }
-            ],
+            layout: defaultLayout,
+            style: defaultStyle,
             elements: graph
         };
     }
@@ -63,8 +71,8 @@ export namespace GraphUtil {
         const edges = graph.filter(elem => elem.group === "edges");
         edges.forEach(e => {
             if(!totals[e.data.target])
-                totals[e.data.target] = { label: e.data.target_label, amount: 0 };
-            totals[e.data.target].amount += e.data.weight || e.data.amount;
+                totals[e.data.target] = { label: e.data.target_label, total: 0 };
+            totals[e.data.target].total += e.data.weight || e.data.amount;
         });
         return totals;
     }
