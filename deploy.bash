78bc34bda6c90f0489103704d61cfe6ccf213ef5
Jonny Brooks <jonathan.leonard.brooks@gmail.com>
2018-09-24 19:20:13 +0100

    Update names to atroneer_item_graph for consistency

--- deploy.bash
+++ deploy.bash
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
+set -e # exit on error
 
 # overwrite remote db
-pg_dump -U postgres -Fc astroneer_tree > astro.db
-pg_restore -h 144.202.8.63 -p 5432 -U postgres -C astro.db --clean -e
-del /Q astro.db
+pg_dump -U postgres -Fc astroneer_item_graph > astro.db
+pg_restore -h 144.202.8.63 -p 5432 -U postgres --clean --dbname=postgres -C astro.db
+rm astro.db
 
 # restart the api service and rebuild the ui
 ssh root@144.202.8.63 /bin/bash << EOF
-    cd /var/www/astroneer_tree
+    cd /var/www/astroneer_item_graph
     git checkout master
     git pull
     npm install
-    sudo systemctl restart astroneer_tree_api.service
+    sudo systemctl restart astroneer_api.service
     npm run build
 EOF
